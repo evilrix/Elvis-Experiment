345c66e730aab04314f806e336aed460d99f1263
evilrix <evilrix@gmail.com>
2020-10-01 15:12:02 +0000

    Initial concept

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 
+#define or_ << elvis_() <<
+
+template<typename T>
 struct elvis
 {
+  T && t;
+
+  template<typename U>
+  friend decltype(auto) operator << (elvis<T> const & e, U && u)
+  {
+    return std::move(
+      static_cast<bool>(e.t) ? e.t : u
+    );
+  }
+};
+
+struct elvis_
+{
+  template<typename T>
+  friend decltype(auto) operator << (T && t, elvis_ const &)
+  {
+    return elvis<T>{ std::move(t) };
+  }
 };
 
 int main()
 {
-  std::cout << "Hello World!\n";
+  auto result = 0 or_ 1;
+  std::cout << ">>" << result << std::endl;
 }
